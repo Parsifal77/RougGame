eabc7d2b705cf9a16c56b576166ab0af9b1acd15
Reywenchh <reformatorz03@gmail.com>
2025-03-23 16:35:35 +0500

    Updated UI
    Hello world

--- Assets/_Scripts/MainMenu.cs
+++ Assets/_Scripts/MainMenu.cs
@@ -1,12 +1,24 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-   public void PlayGame()
+    // Delay in seconds before the scene is loaded.
+    public float delayBeforeLoad = 1f;
+
+    // This method can be linked to your button's OnClick event.
+    public void PlayGame()
     {
+        // Start the coroutine that waits before loading the scene.
+        StartCoroutine(LoadSceneAfterDelay());
+    }
+
+    private IEnumerator LoadSceneAfterDelay()
+    {
+        // Wait for the specified delay.
+        yield return new WaitForSeconds(delayBeforeLoad);
+        // Then load the scene named "RoomContent".
         SceneManager.LoadScene("RoomContent");
     }
 }
