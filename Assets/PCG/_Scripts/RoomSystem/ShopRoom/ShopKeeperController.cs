766d81c618455b481d30becf954d8b467ac256f7
swan3401 <neverchill03@gmail.com>
2025-05-13 19:33:55 +0500

    Shop keeper dialog box

--- Assets/PCG/_Scripts/RoomSystem/ShopRoom/ShopKeeperController.cs
+++ Assets/PCG/_Scripts/RoomSystem/ShopRoom/ShopKeeperController.cs
@@ -4,8 +4,15 @@ public class ShopkeeperController : MonoBehaviour
 {
     [SerializeField]
     private string shopkeeperMessage = "Welcome to my shop! Press B near an item to buy it.";
-
     // This could be expanded with animation, dialog
+
+
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        canvas = transform.Find("Canvas").GetComponent<Canvas>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -14,7 +21,19 @@ public class ShopkeeperController : MonoBehaviour
             // welcome message
             Debug.Log(shopkeeperMessage);
 
+            // TO DO: UI dialog 
+            canvas.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            // hide message
+            Debug.Log("Goodbye!");
             // TO DO: UI dialog box
+            canvas.gameObject.SetActive(false);
         }
     }
 }
