4962dfd6012bd634ee0ef7baf34be972c2a1cb28
Parsifal77 <ilya30112004@gmail.com>
2025-05-13 14:27:03 +0500

    minor changes

--- Assets/_Scripts/ContextSteering/Player/PlayerInput.cs
+++ Assets/_Scripts/ContextSteering/Player/PlayerInput.cs
@@ -39,7 +39,14 @@ public class PlayerInput : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.R))
         {
-            SceneManager.LoadScene(2);
+            if (SceneManager.GetActiveScene().buildIndex == 1)
+            {
+                SceneManager.LoadScene(2);
+            }
+            else
+            {
+                SceneManager.LoadScene(1);
+            }
         }
             
     }
